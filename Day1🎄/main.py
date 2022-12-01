12855aa3a45b230a3f9200ac4642cfa79bde7b8f
Kayla Sween <kaylasween@users.noreply.github.com>
2022-12-01 17:44:38 +0000

    Initial commit

---
+++ Day1🎄/main.py
@@ -0,0 +1,46 @@
+#split by line
+#if empty line, finish list
+
+weights = []
+elfWeight = []
+
+# for line in content:
+#   if (line != "\n"):
+#     elfWeight.append(line)
+#   else:
+#     weights.append(elfWeight)
+#     elfWeight = []
+
+
+with open("input.txt") as f:
+  for line in f:
+    if (line != '\n'):
+      elfWeight.append(int(line.strip('\n')))
+    else:
+      sum = 0
+      for weight in elfWeight:
+        sum = sum + weight
+      weights.append(sum)
+      elfWeight = []
+  
+  newWeights = sorted(weights, reverse=True)
+  print(newWeights)
+
+  threeSum = 0
+
+  for number in newWeights[:3]:
+    print(number)
+    threeSum = threeSum + number
+
+  print(threeSum)
+
+# highestWeight = 0
+
+# for elf in weights:
+#   sum = 0
+#   for weight in elf:
+#     sum = sum + weight
+#   if (sum > highestWeight):
+#     highestWeight = sum
+
+# print(highestWeight)
