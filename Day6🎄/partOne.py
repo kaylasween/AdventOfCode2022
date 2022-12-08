a1a5fbcc756b0a9204e787ab1504441b84474c9e
Kayla Sween <kaylasween@users.noreply.github.com>
2022-12-08 00:49:49 +0000

    Added Day7 empty files, finished Day6

--- Day6🎄/partOne.py
+++ Day6🎄/partOne.py
@@ -2,8 +2,8 @@ input = open("Day6/input.txt").read()
 
 for index, character in enumerate(input):
   possibleStart = []
-  for i in range(14):
+  for i in range(4):
     possibleStart.append(input[index+i])
   if(len(set(possibleStart)) == len(possibleStart)):
-    print("YES!", index+14)
+    print("YES!", index+4)
     break
