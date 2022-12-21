259bd6b74feac00e3285bb3d65d4a98d9a638753
Kayla Sween <kaylasween@users.noreply.github.com>
2022-12-21 18:19:57 +0000

    Wrote some tests for day 8. currently trying to figure out if I can get around using nested for loops on part 2 but honestly, I'm overthinking it and should just roll with it.

--- Day8/partOne.py
+++ Day8/partOne.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+testInput = ["30373", "25512", "65332", "33549", "35390"]
+testGrid = []
+
+for line in testInput: 
+  testGrid.append([*line])
+
+testGrid = np.array(testGrid, dtype=int)
+
 input = open("Day8/input.txt").read()
 input = input.split("\n")
 
@@ -10,19 +18,28 @@ for line in input:
 
 grid = np.array(grid, dtype=int)
 
-visible = 0
+def partOne(inputGrid):
+  visible = 0
+  for y,x in np.ndindex(inputGrid.shape):
+    tree = inputGrid[y, x]
+    west = all(tree > inputGrid[y, :x])
+    east = all(tree > inputGrid[y, x + 1:])
+    north = all(tree > inputGrid[:y, x])
+    south = all(tree > inputGrid[y + 1:, x])
 
-for y,x in np.ndindex(grid.shape):
-  tree = grid[y, x]
-  west = all(tree > grid[y, :x])
-  east = all(tree > grid[y, x + 1:])
-  north = all(tree > grid[:y, x])
-  south = all(tree > grid[y + 1:, x])
+    if(north or south or east or west):
+      visible += 1
+
+  return visible
+
+print(partOne(testGrid))
+print(partOne(grid))
+
+# Tests
+def partOneTest(): 
+  assert partOne(testGrid) == 21
+
+partOneTest()
   
-  if(north or south or east or west):
-    visible += 1
-
-print(visible)
-
 
 
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import numpy as np
-
-input = open("Day8/input.txt").read()
-input = input.split("\n")
+import math
 
 testInput = ["30373", "25512", "65332", "33549", "35390"]
 testGrid = []
-for line in testInput: 
+
+for line in testInput:
   testGrid.append([*line])
 
 testGrid = np.array(testGrid, dtype=int)
+
+print(testGrid)
+
+input = open("Day8/input.txt").read()
+input = input.split("\n")
 
 grid = []
 
@@ -17,24 +21,52 @@ for line in input:
 
 grid = np.array(grid, dtype=int)
 
-print(testGrid)
+def partOne(inputGrid):
+  visible = 0
+  for y, x in np.ndindex(inputGrid.shape):
+    tree = inputGrid[y, x]
+    west = all(tree > inputGrid[y, :x])
+    east = all(tree > inputGrid[y, x + 1:])
+    north = all(tree > inputGrid[:y, x])
+    south = all(tree > inputGrid[y + 1:, x])
 
-visible = 0
-score = 0
+    if (north or south or east or west):
+      visible += 1
 
-for y,x in np.ndindex(testGrid.shape):
-  tree = testGrid[y, x]
-  west = all(tree > testGrid[y, :x])
-  east = all(tree > testGrid[y, x + 1:])
-  north = all(tree > testGrid[:y, x])
-  south = all(tree > testGrid[y + 1:, x])
+  return visible
+
+
+# print(partOne(testGrid))
+# print(partOne(grid))
+
+def partTwo(inputGrid):
+  scores = []
 
   
-  
-  if(north or south or east or west):
-    visible += 1
 
-print(visible)
+  for y, x in np.ndindex(inputGrid.shape):
+    treeScores = []
+    tree = inputGrid[y, x]
+    print(tree)
+    print(inputGrid[y, :x])
+    west = tree > inputGrid[y, :x]
+    east = tree > inputGrid[y, x + 1:]
+    north = tree > inputGrid[:y, x]
+    south = tree > inputGrid[y + 1:, x]
+
+    # if(west):
+    #   treeScores.append()
+
+    scores.append(math.prod(treeScores))
+
+  return max(scores)
+
+# print(partTwo(testGrid))
+
+# Tests
+def test():
+  assert partOne(testGrid) == 21
+  assert partTwo(testGrid) == 8
 
 
-
+test()
